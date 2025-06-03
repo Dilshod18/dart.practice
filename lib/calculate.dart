2356814cff39abd67b7b5fc816995396f32ace55
dilshod-dev <dilshodkavrakowww@gmail.com>
2025-06-03 18:18:14 +0500

    practice function

---
+++ lib/calculate.dart
@@ -0,0 +1,17 @@
+int calculate(
+  int a, int b
+) {
+  return a + b;
+}
+
+String hello(String name) {
+  return "Hello $name!";
+}
+
+void start() {
+print(calculate(10, 30));
+print(hello("Dilshod"));
+print(hello("Faridun"));
+print(hello("Mustafo"));
+
+}
